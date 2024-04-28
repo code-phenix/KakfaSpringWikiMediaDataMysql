39b77aab00ee5a7b82fb5a109e0f3223bff25036
codepheonix <55018311+code-phenix@users.noreply.github.com>
2024-04-28 22:52:23 +0530

    Initial Commit Kafka Producer & Consumer with Real Time Wikimedia data event

---
+++ src/main/java/com/wikimedia_data_to_database/wikimediadatatodataabase/WikimediaDataToDataabaseApplication.java
@@ -0,0 +1,13 @@
+package com.wikimedia_data_to_database.wikimediadatatodataabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikimediaDataToDataabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WikimediaDataToDataabaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wikimedia_data_to_database/wikimediadatatodataabase/WikimediaDataToDataabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wikimedia_data_to_database.wikimediadatatodataabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikimediaDataToDataabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
